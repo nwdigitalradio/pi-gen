5cae449580950f1f4473151a6c5d12718ef411aa
Jeremy McDermond <nh6z@nh6z.net>
2018-03-25 19:04:27 -0700

    Move 01-configure-apt to 00-configure-apt to match Raspian.

--- stage0/00-configure-apt/00-run.sh
+++ stage0/00-configure-apt/00-run.sh
@@ -2,6 +2,8 @@
 
 install -m 644 files/sources.list "${ROOTFS_DIR}/etc/apt/"
 install -m 644 files/raspi.list "${ROOTFS_DIR}/etc/apt/sources.list.d/"
+install -m 644 files/compass.list ${ROOTFS_DIR}/etc/apt/sources.list.d/
+install -m 644 files/compass ${ROOTFS_DIR}/etc/apt/preferences.d/
 
 if [ -n "$APT_PROXY" ]; then
 	install -m 644 files/51cache "${ROOTFS_DIR}/etc/apt/apt.conf.d/51cache"
@@ -11,6 +13,7 @@ else
 fi
 
 on_chroot apt-key add - < files/raspberrypi.gpg.key
+on_chroot apt-key add - < files/compass.gpg.key
 on_chroot << EOF
 apt-get update
 apt-get dist-upgrade -y
